1b8551f37d19d9b3c9fea84a3620e801452a0c0c
younero <45897811+younero@users.noreply.github.com>
2020-10-09 12:14:33 +0800

    update

--- chapter3/3.3.3/stack.py
+++ chapter3/3.3.3/stack.py
@@ -17,8 +17,8 @@ y3 = [float(x) for x in datax[3]]
 
 bar.add_xaxis(x)
 bar.add_yaxis('A', y1, stack=1)
-bar.add_yaxis('B', y1, stack=1)
-bar.add_yaxis('C', y1, stack=1)
+bar.add_yaxis('B', y2, stack=1)
+bar.add_yaxis('C', y3, stack=1)
 bar.set_series_opts(label_opts=opt.LabelOpts(is_show=False))
 
 bar.render('stack.html')
